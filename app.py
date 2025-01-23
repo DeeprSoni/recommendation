feddd8136eb7ed980789f66db23050310ce225c2
Deep Soni <62744688+DeeprSoni@users.noreply.github.com>
2025-01-23 14:47:00 +0530

    Update app.py for open sourcing and sparse algo

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
-import recommendation
-import os  # Importing the os module
+import os
+from recommendation.data_handler import load_dataset, copy_default_dataset
+from recommendation.similarity import get_recommendation_for_item
 
 # Get the absolute path to the directory containing app.py
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -16,16 +17,13 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         csv_file = request.files.get("csvfile")
-        
+
         # Check if a file was uploaded
         if csv_file:
             csv_file.save(CSV_PATH)  # Save the uploaded CSV to a permanent location
         else:
             # Use the default CSV if no file was uploaded
-            # testing git 
-            with open(DEFAULT_CSV_PATH, "rb") as df:
-                with open(CSV_PATH, "wb") as ef:
-                    ef.write(df.read())
+            copy_default_dataset(DEFAULT_CSV_PATH, CSV_PATH)
 
         return redirect(url_for('get_recommendation'))
     else:
@@ -34,22 +32,20 @@ def index():
 @app.route("/use-default", methods=["GET"])
 def use_default():
     # Copy the default CSV to the CSV_PATH
-    with open(DEFAULT_CSV_PATH, "rb") as df:
-        with open(CSV_PATH, "wb") as ef:
-            ef.write(df.read())
+    copy_default_dataset(DEFAULT_CSV_PATH, CSV_PATH)
     return redirect(url_for('get_recommendation'))
 
 @app.route("/recommend", methods=["GET", "POST"])
 def get_recommendation():
-    data = pd.read_csv(CSV_PATH)  # Read the saved CSV file
+    data = load_dataset(CSV_PATH)  # Read the saved CSV file
 
     recommended = None
     item_to_recommend = None
 
     if request.method == "POST":
         item_to_recommend = request.form["item"]
-        recommended = [recommendation.get_recommendation_for_item(item_to_recommend, data)]
-    
+        recommended = [get_recommendation_for_item(item_to_recommend, data)]
+
     return render_template("index.html", recommended=recommended, item=item_to_recommend)
 
 if __name__ == "__main__":
